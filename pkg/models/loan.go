cde3bec88dcd667f82add9d2110b477d26b95637
gilangmahardhika <gilangmahardhika@gmail.com>
2025-02-02 13:41:48 +0700

    feat: generate agreement document link

--- pkg/models/loan.go
+++ pkg/models/loan.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"gorm.io/gorm"
@@ -17,11 +18,11 @@ type Loan struct {
 	AgreementLink             *string      `gorm:"default:null" json:"agreement_link"`
 	State                     string       `gorm:"not null;default:'proposed'" json:"state"`
 	Investments               []Investment `gorm:"foreignKey:LoanID" json:"investments,omitempty"`
-	ApprovedAt                *time.Time   `gorm:"not null;default:null" json:"approved_at"`
-	ApprovedBy                *string      `gorm:"not null;default:null" json:"approved_by"`
-	VisitProof                *string      `gorm:"not null;default:null" json:"visit_proof"`
-	DisbursedAt               *time.Time   `gorm:"not null;default:null" json:"disbursed_at"`
-	DisbursedBy               *string      `gorm:"not null;default:null" json:"disbursed_by"`
+	ApprovedAt                *time.Time   `gorm:"default:null" json:"approved_at"`
+	ApprovedBy                *string      `gorm:"default:null" json:"approved_by"`
+	VisitProof                *string      `gorm:"default:null" json:"visit_proof"`
+	DisbursedAt               *time.Time   `gorm:"default:null" json:"disbursed_at"`
+	DisbursedBy               *string      `gorm:"default:null" json:"disbursed_by"`
 	CreatedAt                 time.Time    `gorm:"not null;autoCreateTime" json:"created_at"`
 	UpdatedAt                 time.Time    `gorm:"not null;autoUpdateTime" json:"updated_at"`
 }
@@ -65,4 +66,12 @@ func (l *Loan) BeforeSave(tx *gorm.DB) error {
 			l.State)
 	}
 	return nil
+}
+
+// Generate link if load set to approved
+func (l *Loan) GenerateLink() {
+	if l.State == "approved" {
+		link := "https://example.com/loan/" + strconv.Itoa(int(l.ID))
+		l.AgreementLink = &link
+	}
 }
